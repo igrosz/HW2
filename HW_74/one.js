415e5385376d6db43736d298a6d7e51563add785
igrosz <isaacgrosz@gmail.com>
2017-09-14 22:01:02 -0400

    HW_74

---
+++ HW_74/one.js
@@ -0,0 +1,14 @@
+alert(name);
+
+
+function greaterThan(a,b) {
+    if(a>b){
+        return true;
+    }
+    else{
+        return false;
+    }
+} 
+
+console.log(greaterThan(5,6));
+console.log(greaterThan(6,5));
