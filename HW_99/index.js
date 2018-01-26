0971a1a6d538bb9c8b4517f5e1ac96402b336eb9
igrosz <isaacgrosz@gmail.com>
2018-01-25 22:51:17 -0500

    hw 99

---
+++ HW_99/index.js
@@ -0,0 +1,10 @@
+
+
+'use strict';
+
+//const $ = require('jQuery');
+import $ from 'jquery';
+
+var theDiv = $('#theDiv');
+theDiv.css("background-color", "lightgray");
+theDiv.css("color", "green");
